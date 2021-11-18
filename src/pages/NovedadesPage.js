ae95f2ccea69d3bba8d5eccffa5ab908b10b51fe
maguero86 <86382159+maguero86@users.noreply.github.com>
2021-11-18 19:56:12 -0300

    Add files via upload

--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NovedadItem from '..components/novedades/novedadItem'
 
 const NovedadesPage = (props) =>{
+	const [loading, setLoading] = useState(false);
+	const [novedades, setNovedades] = useState([]);
+	
+	useEffect(() => {
+		const cargarNovedades = async() => {
+			setLoading(true);
+			const response = await axios.get('http://localhost:3000/api/novedades');
+			setNovedades(response.data);
+			setLoading(false);
+		};
+		cargarNovedades();
+	}, []);
+	
 	return (
 		<section className="holder">				
 			<h2>Novedades</h2>
-			<h3>titulo</h3>
-			<h4>subtitulo</h4>
-			<p>
-			cuerpo
-			</p>	
+			{loading ? (<p>Cargando...</p>) : (novedades.map(item => <NovedadItem key={item.id} title={item.titulo} subtitle={item.subtitulo} imagen={item.imagen} body={item.cuerpo}/>))}				
 		</section>
 	);
 }
